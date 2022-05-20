c06a36744e64675102420766385af1141d147588
Rafael Garcia <82184594+slodead@users.noreply.github.com>
2022-05-20 20:51:56 -0300

    Atualizando o init do projeto

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-conseole.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("A aplicação está se conectando ao banco de dados")
